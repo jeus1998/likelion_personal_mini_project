94a3f3171243909769509edbf11fabfb0f7bbd43
baejeu12 <baejeu@naver.com>
2023-06-29 11:40:18 +0900

    first commit

---
+++ src/main/java/com/example/miniproject_basic_baejeu/MiniProjectBasicBaejeuApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniproject_basic_baejeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectBasicBaejeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectBasicBaejeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniproject_basic_baejeu/MiniProjectBasicBaejeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproject_basic_baejeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectBasicBaejeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
